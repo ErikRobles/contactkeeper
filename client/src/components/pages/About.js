947e953100f3b9ba618e0e922e908c9797107539
ErikRobles <erikjames69@hotmail.com>
2021-11-16 16:14:01 -0600

    added edit functionality

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className='my-1'>
+        This is a full stack application for managing Contacts
+      </p>
+      <p className='bg-dark p'>
+        {' '}
+        <strong>Version</strong> 1.0.3
+      </p>
+    </div>
+  );
+};
+
+export default About;
